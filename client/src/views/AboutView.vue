7fa4a8bbf124561ed3fdf7e46088c8cb3c232053
Fiona Kennedy <kennedyf2@newpaltz.edu>
2023-10-24 09:41:28 -0400

    updated about

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -5,8 +5,8 @@
 <template>
     <div>
         <h1 class="title ml-3">About Page</h1>
-        <h3 class="subtitle has-text-centered">Fiona Kennedy is the Admin account</h3>
-        <h3 class="subtitle has-text-centered">Marceline Abadeer and Jason Vorhees are User accounts</h3>
+        <h3 class="subtitle has-text-centered">Fiona Kennedy: Admin account</h3>
+        <h3 class="subtitle has-text-centered">Marceline Abadeer + Jason Vorhees: User accounts</h3>
     </div>
 </template>
 
